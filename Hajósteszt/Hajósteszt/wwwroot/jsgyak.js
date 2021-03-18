983b42a9e860cd284c06f9a7d1d8c452bef3c40e
Máté Turner <mate.turner@stud.uni-corvinus.hu>
2021-03-18 18:14:36 +0100

    jsgyak

---
+++ Hajósteszt/Hajósteszt/wwwroot/jsgyak.js
@@ -0,0 +1,39 @@
+﻿
+
+let hova = document.getElementById("ide")
+hova.innerHTML = ""
+for (var i = 0; i < 10; i++) {
+    var sor = document.createElement("div")
+    hova.appendChild(sor)
+    sor.innerText = i
+    sor.style.background = `rgb(${30 + (30 * i)},0,0)`
+    sor.classList.add("doboz")
+}
+let pascal=document.getElementById("pascal")
+
+for (var sor = 0; sor < 10; sor++) {
+    var divsor = document.createElement("div")
+    divsor.classList.add("sor")
+    pascal.appendChild(divsor)
+
+    for (var oszlop = 0; oszlop <= sor; oszlop++) {
+        var divelem = document.createElement("div")
+        divelem.classList.add("elem")
+        divsor.appendChild(divelem)
+        
+
+        var faktoriális = function (n) {
+            let er = 1;
+            for (let i = 2; i <= n; i++) {
+                er = er * i;
+            }
+            return er;
+        }
+        divelem.innerText = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor - oszlop))
+        
+    }
+}
+
+
+
+
